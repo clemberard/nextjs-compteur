17c7dc3e71f1ec0bedd51a4e62a3dd105795490c
Clement Berard <clement.berard@ecole-isitech.fr>
2024-05-14 11:39:22 +0200

    remove decrementation negative

--- compteur/components/compteur.jsx
+++ compteur/components/compteur.jsx
@@ -8,11 +8,11 @@ function displayCount() {
 			<button className={styles.bouton} onClick={() => setCount(count + 1)}>
 				Clique sur moi pour incrémenter
 			</button>
-			<button className={styles.bouton} onClick={() => setCount(count - 1)}>
+			<button className={styles.bouton} onClick={() => (count > 0 ? setCount(count - 1) : setCount(0))}>
 				Clique sur moi pour décrémenter
 			</button>
 			<button className={styles.bouton} onClick={() => setCount(0)}>
-				Tu veux me reset ? :'(
+				Tu veux me reset ? &#128546;
 			</button>
 			<p>Voici le nombre de clics : {count}</p>
 		</>
